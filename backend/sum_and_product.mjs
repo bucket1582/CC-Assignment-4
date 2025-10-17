2b19a21fbaafc5ceabf560f4b6e7d236c50f0dc0
bucket1582 <sout0811@naver.com>
2025-10-17 22:21:46 +0900

    Fix: json format

--- backend/sum_and_product.mjs
+++ backend/sum_and_product.mjs
@@ -1,30 +1,24 @@
 export const handler = async (event, context) => {
-    const { x: qs_x, y: qs_y } = event.queryStringParameters || {};
-    const x = Number(qs_x);
-    const y = Number(qs_y);
-    if (isNaN(x)) {
-        return {
-            statusCode: 400,
-            body: JSON.stringify({
-                "type": "error",
-                "message": "x should be a number"
-            })
-        };
-    } 
-    
-    if (isNaN(y)) {
-        return {
-            statusCode: 400,
-            body: JSON.stringify({
-                "type": "error",
-                "message": "y should be a number"
-            })
-        };
-    }
+  const { x: qs_x, y: qs_y } = event.queryStringParameters || {};
+  const x = Number(qs_x);
+  const y = Number(qs_y);
+  if (isNaN(x)) {
+      return {
+        "type": "error",
+        "message": "x should be a number"
+      };
+  } 
+  
+  if (isNaN(y)) {
+      return {
+        "type": "error",
+        "message": "y should be a number"
+      };
+  }
 
-    return {
-        "type": "success",
-        "sum": x + y,
-        "prod": x * y
-    };
+  return {
+      "type": "success",
+      "sum": x + y,
+      "prod": x * y
+  };
 }
